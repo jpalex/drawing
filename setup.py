13817bdddddabf3cc3fd71f9ae293dfa69fd6e06
jpalex <aluno80433@ipt.pt>
2023-05-23 16:06:27 +0100

    requiremenst file and readme added

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import os
-
-from setuptools import setup, find_packages
 setup(
     name="drawing",
     version="0.1",
     description="Drawing Distribution Package",
-    packages=find_packages(),
-    include_package_data=True
+    long_description=long_description,
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    install_requires=install_requires
 )
