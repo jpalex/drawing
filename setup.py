2cd53a3a55ea0d246c7c7da78ddb31af77e16a25
jpalex <aluno80433@ipt.pt>
2023-05-23 16:36:36 +0100

    create advanced sub package

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+from codecs import open
+from os.path import join, abspath, dirname
+from setuptools import setup, find_packages
+import os
+
+requirementPath = 'requirements.txt'
+install_requires = []
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+here = abspath(dirname(__file__))
+
+# Get the long description from the README file
+with open(join(here, 'README.md'), encoding='utf-8') as buff:
+    long_description = buff.read()
+
 setup(
     name="drawing",
     version="0.1",
